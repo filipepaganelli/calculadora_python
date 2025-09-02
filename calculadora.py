be3e8ed25497f9fd993f4cc1a5a484b15a26e032
filipepaganelli <paganelli.filipe@gmail.com>
2025-09-02 16:30:13 -0300

    Modularizar tratamento de inputs do menu

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,8 @@
+import os
+import time
+import sys
+
+
 
 #Funções da operações
 class Calculadora():
@@ -24,53 +29,70 @@ class Calculadora():
                 numero = int(input(mensagem))
                 if numero > 0:
                     return numero
+           
                 else:
-                    print("O número digitado deve ser maior do que 0! ")
+                    print(f"O número digitado deve ser maior do que {valor_minimo}! ")
             except ValueError:
                 print("Entrada inválida! Digite um número inteiro!")
 
+def efeito_digitacao(texto):
+    """
+    Simula o efeito de digitação em uma linha de texto.
+    """
+    for caractere in texto:
+        sys.stdout.write(caractere)  # Imprime um caractere sem pular linha
+        sys.stdout.flush()           # Força a impressão imediata
+        time.sleep(0.05)  
+
+def menu():
+    while True:       
+        try:
+            os.system("cls")
+            opcao = int(input("""
+            Qual operação deseja fazer?\n                 
+            1-Somar
+            2-Substrair
+            3-Multiplicar
+            4-Dividir
+            5-Raiz                          
+            0-Sair\n
+            >>>Digite aqui: """))
+
+            if opcao not in (0,1,2,3,4,5):
+                efeito_digitacao("\n ATENÇÃO!! Digite apenas um dos números do menu!")
+                time.sleep(0.5)
+                continue
+            
+            else:
+                return opcao
+                
+        except ValueError:
+            print('Letras ou caracteres não são aceitáveis. Digite uma opção válida!')
+            os.system("cls")
+            continue
+
+
 def rodar_calculadora():
     while True:
-        try:
-            opcao = int(input("""
-    Qual operação deseja fazer?\n                 
-    1-Somar
-    2-Substrair
-    3-Multiplicar
-    4-Dividir
-    5-Raiz                          
-    0-Sair\n
-    >>>Digite aqui: """))
-        except ValueError:
-            print('Letras não são aceitáveis. Digite uma opção válida!')
-            continue
+        opcao = menu()
         if opcao ==0:
-            print("\nObrigado por usar nossa calculadora!")
-            break
+            efeito_digitacao("\nObrigado por usar nossa calculadora! ;)")
+            return 0
+        #Pedindo os valore que serão calculados 
+        v1 = Calculadora.validar_input("Insira o primeiro valor:", 0)
+        v2 = Calculadora.validar_input("Insira o segundo valor:", 0)  
 
-
-        if opcao not in (1,2,3,4,5):
-            print(f"\n {opcao} não é está no menu. Digite uma opção válida!")
-            continue
-
-
-            
-
-        if opcao in (1,2,3,4):
-            v1 = int(input("Insira o primeiro valor:"))
-            v2 = int(input("Insira o segundo valor:"))  
-
-            if opcao == 1:
-                Calculadora.somar(v1,v2)
-            elif opcao ==2:
-                Calculadora.subtrair(v1,v2)
-            elif opcao ==3:
-                Calculadora.multiplicar(v1,v2)
-            elif opcao ==4:
-                while v2 ==0:
-                    print('Não é possível devidir por 0.')
-                    v2 = int(input("Insira o segundo valor novamente: "))  
-                Calculadora.dividir(v1,v2)
+        if opcao == 1:
+            Calculadora.somar(v1,v2)
+        elif opcao ==2:
+            Calculadora.subtrair(v1,v2)
+        elif opcao ==3:
+            Calculadora.multiplicar(v1,v2)
+        elif opcao ==4:
+            while v2 ==0:
+                print('Não é possível devidir por 0.')
+                v2 = int(input("Insira o segundo valor novamente: "))  
+            Calculadora.dividir(v1,v2)
         elif opcao == 5:
                 while True:
                     v1 = int(input("Insira o coeficiente da raiz(Ex:2,3,4...):\n "))
@@ -85,9 +107,11 @@ def rodar_calculadora():
                         continue
                     break
                 Calculadora.raiz(v1,v2)
+
         
 
-    print()
+if __name__ == "__main__":
 
-
+ 
+    rodar_calculadora()
 
